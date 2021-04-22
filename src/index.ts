568136d193b9a480067ca2524eb4e4dc177b5672
Adam Kamboj <kamboj.adam@gmail.com>
2021-04-22 14:04:32 -0400

    added graphql schema and post resolver

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import microConfig from "./mikro-orm.config";
+import mikroConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
-  const orm = await MikroORM.init(microConfig);
+  const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(9999, () => {
+    console.log("server started on localhost:9999");
+  });
 };
 
 main().catch((err) => {
