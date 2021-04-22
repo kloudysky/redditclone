bf35ea71d0a26ddd017a0857d01be99afe3abcc1
Adam Kamboj <kamboj.adam@gmail.com>
2021-04-22 17:12:41 -0400

    :sparkles: added session cookie to login and register

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { PostResolver } from "./resolvers/post";
 import { UserResolver } from "./resolvers/user";
+import redis from "redis";
+import session from "express-session";
+import connectRedis from "connect-redis";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
@@ -14,12 +17,34 @@ const main = async () => {
 
   const app = express();
 
+  const RedisStore = connectRedis(session);
+  const redisClient = redis.createClient();
+
+  app.use(
+    session({
+      name: "qid",
+      store: new RedisStore({
+        client: redisClient,
+        disableTouch: true,
+      }),
+      cookie: {
+        maxAge: 999 * 60 * 60 * 24 * 365 * 10, //10 years
+        httpOnly: true,
+        secure: __prod__, //cookie only works in https
+        sameSite: "lax", //csrf
+      },
+      saveUninitialized: false,
+      secret: "B24C733FFC6E3B8824C1BE4B4ABCC",
+      resave: false,
+    })
+  );
+
   const apolloServer = new ApolloServer({
     schema: await buildSchema({
       resolvers: [PostResolver, UserResolver],
       validate: false,
     }),
-    context: () => ({ em: orm.em }),
+    context: ({ req, res }) => ({ em: orm.em, req, res }),
   });
 
   apolloServer.applyMiddleware({ app });
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -7,6 +7,7 @@ import {
   InputType,
   Mutation,
   ObjectType,
+  Query,
   Resolver,
 } from "type-graphql";
 import argon2 from "argon2";
@@ -38,24 +39,69 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
-  @Mutation(() => User)
+  @Query(() => User, { nullable: true })
+  async me(@Ctx() { req, em }: MyContext) {
+    if (!req.session.userId) {
+      return null;
+    }
+    const user = await em.findOne(User, { id: req.session.userId });
+    return user;
+  }
+
+  @Mutation(() => UserResponse)
   async register(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
-  ) {
+    @Ctx() { em, req }: MyContext
+  ): Promise<UserResponse> {
+    if (options.username.length < 3) {
+      return {
+        errors: [
+          {
+            field: "username",
+            message: "Username must be at least 3 characters",
+          },
+        ],
+      };
+    }
+    if (options.password.length < 6) {
+      return {
+        errors: [
+          {
+            field: "password",
+            message: "Password must be at least 6 characters",
+          },
+        ],
+      };
+    }
     const hashedPassword = await argon2.hash(options.password);
     const user = em.create(User, {
       username: options.username,
       password: hashedPassword,
     });
-    await em.persistAndFlush(user);
-    return user;
+    try {
+      await em.persistAndFlush(user);
+    } catch (error) {
+      //dup username error
+      if (error.code === "23505") {
+        //|| error.detail.includes("already exists")) {
+        return {
+          errors: [
+            {
+              field: "username",
+              message: "Username already exists",
+            },
+          ],
+        };
+      }
+    }
+    req.session!.userId = user.id;
+    return { user };
   }
 
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, { username: options.username });
     if (!user) {
@@ -79,6 +125,9 @@ export class UserResolver {
         ],
       };
     }
+
+    req.session!.userId = user.id;
+
     return {
       user,
     };
